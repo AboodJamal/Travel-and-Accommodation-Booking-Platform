2354bc0a158c03adcd31c2ad4c8676c38b967642
Abood <abood.jamal550@gmail.com>
2024-12-30 00:59:53 +0200

    add DbContext , Configurations and DB-connections string

---
+++ TABP.Infrastructure/Configurations/PaymentEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using TABP.Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Configurations;
+
+public class PaymentEntityConfiguration: IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder
+            .Property(payment => payment.Status)
+            .IsRequired()
+            .HasConversion(new EnumToStringConverter<PaymentStatus>());
+            
+        builder
+            .Property(payment => payment.Method)
+            .IsRequired()
+            .HasConversion(new EnumToStringConverter<PaymentMethod>());
+
+        builder
+            .HasIndex(payment => payment.Status);
+
+        builder
+            .HasIndex(payment => payment.Method);
+    }
+}

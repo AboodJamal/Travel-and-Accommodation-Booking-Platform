907d50ea2f782c678d40087edc2d1745e0422457
Abood <abood.jamal550@gmail.com>
2024-12-29 03:52:54 +0200

    -

--- TABP.Tests/obj/Debug/net8.0/TABP.Tests.AssemblyInfo.cs
+++ TABP.Tests/obj/Debug/net8.0/TABP.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TABP.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428918617f149374e1f78fe0c9cacde965d4ff2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb10cbae25f800aea63d9bfd93366a6fdab46dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TABP.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TABP.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

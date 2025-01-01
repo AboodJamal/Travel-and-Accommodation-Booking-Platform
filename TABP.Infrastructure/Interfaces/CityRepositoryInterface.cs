5c344b40b97ed043b0c17b9f38a44e14235859e8
Abood <abood.jamal550@gmail.com>
2025-01-01 19:48:27 +0200

    add Repositories Interfaces ,Exceptions and some models

---
+++ TABP.Infrastructure/Interfaces/CityRepositoryInterface.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.ExtraModels;
+using TABP.Domain.Entities;
+namespace Infrastructure.Interfaces;
+
+public interface CityRepositoryInterface
+{
+    public Task<PaginatedList<City>>
+    GetAllAsync(bool includeHotels, string? searchQuery, int pageNumber, int pageSize);
+    public Task<City?> GetByIdAsync(Guid cityId, bool includeHotels);
+    public Task<City?> InsertAsync(City city);
+    public Task UpdateAsync(City city);
+    public Task DeleteAsync(Guid cityId);
+    public Task<List<City>> GetTrendingCitiesAsync(int count);
+    public Task SaveChangesAsync();
+    public Task<bool> IsExistAsync(Guid cityId);
+}
---
+++ TABP.Infrastructure/Interfaces/DiscountRepositoryInterface.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.ExtraModels;
+using TABP.Domain.Entities;
+namespace Infrastructure.Interfaces;
+
+public interface DiscountRepositoryInterface
+{
+    public Task<PaginatedList<Discount>>
+    GetAllByRoomTypeIdAsync(Guid roomTypeId, int pageNumber, int pageSize);
+    public Task<Discount?> GetByIdAsync(Guid discountId);
+    public Task<bool> HasOverlappingDiscountAsync(Guid roomTypeId, 
+        DateTime fromDate,
+        DateTime toDate);
+    public Task<Discount?> InsertAsync(Discount discount);
+    public Task DeleteAsync(Guid discountId);
+    public Task SaveChangesAsync();
+    public Task<bool> IsExistAsync(Guid discountId);
+}

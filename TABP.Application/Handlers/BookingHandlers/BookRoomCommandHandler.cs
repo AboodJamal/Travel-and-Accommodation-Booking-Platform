c10e523c0bdb1d9443df919efd85c08394cf0eea
Abood <abood.jamal550@gmail.com>
2025-01-03 00:54:36 +0200

    add initial Booking Handlers

---
+++ TABP.Application/Handlers/BookingHandlers/BookRoomCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Commands.BookingCommands;
+using TABP.Application.DTOs.BookingDtos;
+using AutoMapper;
+using Infrastructure.Interfaces;
+using TABP.Domain.Entities;
+using MediatR;
+
+namespace Application.Handlers.BookingHandlers;
+
+public class BookRoomCommandHandler : IRequestHandler<BookRoomCommand, BookingDto?>
+{
+    private readonly BookingRepositoryInterface _bookingRepository;   
+    private readonly UserRepositoryInterface _userRepository;   
+    private readonly RoomRepositoryInterface _roomRepository;   
+    private readonly IMapper _mapper;                                                   
+
+    public BookRoomCommandHandler(BookingRepositoryInterface bookingRepository, IMapper mapper, UserRepositoryInterface userRepository, RoomRepositoryInterface roomRepository)
+    {
+        _bookingRepository = bookingRepository;
+        _mapper = mapper;
+        _userRepository = userRepository;
+        _roomRepository = roomRepository;
+    }
+
+    public async Task<BookingDto?> Handle(BookRoomCommand request, CancellationToken cancellationToken)
+    {
+        var bookingToAdd = _mapper.Map<Booking>(request);
+        bookingToAdd.UserId = await _userRepository.GetGuestIdByEmailAsync(request.GuestEmail);
+        bookingToAdd.Price = await _roomRepository.GetPriceForRoomWithDiscount(request.RoomId);
+        var addedBooking = await _bookingRepository.InsertAsync(bookingToAdd);
+        return _mapper.Map<BookingDto>(addedBooking);
+    }
+}
---
+++ TABP.Application/Handlers/BookingHandlers/GetBookingsByHotelIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using TABP.Application.DTOs.BookingDtos;
+using Application.Queries.BookingQueries;
+using AutoMapper;
+using Infrastructure.Interfaces;
+using Infrastructure.ExtraModels;
+using MediatR;
+
+namespace Application.Handlers.BookingHandlers;
+
+public class GetBookingsByHotelIdQueryHandler : IRequestHandler<GetBookingsByHotelIdQuery, PaginatedList<BookingDto>>
+{
+    private readonly BookingRepositoryInterface _bookingRepository;
+    private readonly IMapper _mapper;
+
+    public GetBookingsByHotelIdQueryHandler(BookingRepositoryInterface bookingRepository, IMapper mapper)
+    {
+        _bookingRepository = bookingRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<BookingDto>> Handle(GetBookingsByHotelIdQuery request, CancellationToken cancellationToken)
+    {
+        var paginatedList = await
+            _bookingRepository
+            .GetAllByHotelIdAsync(
+                request.HotelId,
+                request.PageNumber,
+                request.PageSize);
+        
+        return new PaginatedList<BookingDto>(
+            _mapper.Map<List<BookingDto>>(paginatedList.Items),
+            paginatedList.PageData);
+    }
+}

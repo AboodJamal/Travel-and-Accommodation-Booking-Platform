6335fc03cb81b543520fe40baf285daa4fad9f9b
Abood <abood.jamal550@gmail.com>
2025-01-03 20:37:35 +0200

    add RoomCategoryHandlers and RoomHandlers

---
+++ TABP.Application/Handlers/RoomCategoryHandlers/CheckRoomTypeExistsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.RoomCategoryQueries;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.RoomCategoryHandlers;
+
+public class CheckRoomTypeExistsQueryHandler : IRequestHandler<CheckRoomTypeExistsQuery, bool>
+{
+    private readonly RoomTypeRepositoryInterface _roomTypeRepository;
+
+    public CheckRoomTypeExistsQueryHandler(RoomTypeRepositoryInterface roomTypeRepository)
+    {
+        _roomTypeRepository = roomTypeRepository;
+    }
+
+    public Task<bool> Handle(CheckRoomTypeExistsQuery request, CancellationToken cancellationToken)
+    {
+        return _roomTypeRepository.IsExistAsync(request.Id);
+    }
+}
---
+++ TABP.Application/Handlers/RoomHandlers/GetRoomByIdAndHotelIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using TABP.Application.DTOs.RoomDtos;
+using Application.Queries.RoomQueries;
+using AutoMapper;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.RoomHandlers;
+
+public class GetRoomByIdQueryHandler : IRequestHandler<GetRoomByIdQuery,RoomDto?>
+{
+    private readonly RoomRepositoryInterface _roomRepository;
+    private readonly IMapper _mapper;
+
+    public GetRoomByIdQueryHandler(RoomRepositoryInterface roomRepository, IMapper mapper)
+    {
+        _roomRepository = roomRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<RoomDto?> Handle(GetRoomByIdQuery request,
+    CancellationToken cancellationToken)
+    {
+        var room = await _roomRepository.GetByIdAsync(request.RoomId);
+        return _mapper.Map<RoomDto>(room);
+    }
+}

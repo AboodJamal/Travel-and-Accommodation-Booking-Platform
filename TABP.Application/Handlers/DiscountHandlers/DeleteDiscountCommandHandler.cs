17999f8a7b385cbbc9b43450f9fbf6a47fe8ab0b
Abood <abood.jamal550@gmail.com>
2025-01-04 23:32:39 +0200

    add DiscountHandlers

---
+++ TABP.Application/Handlers/DiscountHandlers/DeleteDiscountCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Commands.DiscountCommands;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.DiscountHandlers;
+
+public class DeleteDiscountCommandHandler : IRequestHandler<DeleteDiscountCommand>
+{
+    private readonly DiscountRepositoryInterface _discountRepository;
+
+    public DeleteDiscountCommandHandler(DiscountRepositoryInterface discountRepository)
+    {
+        _discountRepository = discountRepository;
+    }
+
+    public async Task Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        await _discountRepository.DeleteAsync(request.Id);
+    }
+}

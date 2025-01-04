64a07a6991bc228d384ad4098adfa69f2c14343e
Abood <abood.jamal550@gmail.com>
2025-01-04 23:33:52 +0200

    add RoomAmenityHandlers

---
+++ TABP.Application/Handlers/RoomAmenityHandlers/CheckIfRoomAmenityExistsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.RoomAmenityQueries;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.RoomAmenityHandlers;
+
+public class CheckIfRoomAmenityExistsQueryHandler : IRequestHandler<CheckIfRoomAmenityExistsQuery, bool>
+{
+    private readonly RoomAmenityRepositoryInterface _roomAmenityRepository;
+
+    public CheckIfRoomAmenityExistsQueryHandler(RoomAmenityRepositoryInterface amenityRepository)
+    {
+        _roomAmenityRepository = amenityRepository;
+    }
+
+    public async Task<bool> Handle(CheckIfRoomAmenityExistsQuery request, CancellationToken cancellationToken)
+    {
+        return await _roomAmenityRepository.IsExistAsync(request.Id);
+    }
+}
---
+++ TABP.Application/Handlers/RoomAmenityHandlers/DeleteRoomAmenityCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Commands.RoomAmenityCommands;
+using AutoMapper;
+using Infrastructure.Interfaces;
+using Infrastructure.Exceptions;
+using MediatR;
+
+namespace Application.Handlers.RoomAmenityHandlers;
+
+public class DeleteRoomAmenityCommandHandler : IRequestHandler<DeleteRoomAmenityCommand>
+{
+    private readonly RoomAmenityRepositoryInterface _roomAmenityRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteRoomAmenityCommandHandler(RoomAmenityRepositoryInterface roomAmenityRepository)
+    {
+        _roomAmenityRepository = roomAmenityRepository;
+    }
+    
+    public async Task Handle(DeleteRoomAmenityCommand request, CancellationToken cancellationToken)
+    {
+        if (!await _roomAmenityRepository.IsExistAsync(request.Id))
+        {
+            throw new NotFoundException("Room Amenity Doesn't Exists To Delete");
+        }
+        await _roomAmenityRepository.DeleteAsync(request.Id);
+    }
+}

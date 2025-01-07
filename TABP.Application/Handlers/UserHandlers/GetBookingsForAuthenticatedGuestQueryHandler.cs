4e59ad3c70b17bfa02b78a9a6a03153b3c132186
Abood <abood.jamal550@gmail.com>
2025-01-08 01:45:47 +0200

    add UserHandlers and PasswordHandlers

---
+++ TABP.Application/Handlers/UserHandlers/GetBookingsForAuthenticatedGuestQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using TABP.Application.DTOs.BookingDtos;
+using Application.Queries.UserQueries;
+using AutoMapper;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.UserHandlers;
+
+public class GetBookingsForAuthenticatedGuestQueryHandler :
+    IRequestHandler<GetBookingsForAuthenticatedGuestQuery, List<BookingDto>>
+{
+    private readonly UserRepositoryInterface _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetBookingsForAuthenticatedGuestQueryHandler(UserRepositoryInterface userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<BookingDto>> Handle(GetBookingsForAuthenticatedGuestQuery request, CancellationToken cancellationToken)
+    {
+        return _mapper.Map<List<BookingDto>>
+        (await _userRepository
+        .GetBookingsForSpecificAuthenticatedGuestAsync
+        (request.Email, request.Count));
+    }
+}
---
+++ TABP.Application/Utils/PasswordHandlerInterface.cs
@@ -0,0 +1,7 @@
+﻿namespace TABP.Application.Utils;
+
+public interface PasswordHandlerInterface
+{
+    public string? GenerateHashedPassword(string password, byte[] salt);
+    public byte[] GenerateHashingSaltValue(); // arr of bytes
+}
